ab6e41b3444e63757e63aced9ae28256fe27d00c
hailong <hailengc@126.com>
2018-11-04 22:39:09 +0800

    use hooks to create and keep folder

--- lib/jekyll/miniaudio.rb
+++ lib/jekyll/miniaudio.rb
@@ -9,21 +9,29 @@ class MiniAudio < Liquid::Tag
   end
 
   def render(_context)
-    working_assets_path = File.join Dir.pwd, 'assets', "miniaudio-#{Jekyll::Miniaudio::VERSION}"
     ma_path = File.join File.expand_path(__dir__), 'miniaudio'
-    unless Dir.exist?(working_assets_path)
-      src_asset_path = File.join ma_path, 'h5audio'
-      FileUtils.cp_r(src_asset_path, working_assets_path)
-    end
-
     template = File.read(File.join(ma_path, 'h5audio', 'template.html'))
     Liquid::Template.parse(template).render(
       'audioSrc' => @audio_url,
-      'title' => File.basename(@audio_url, ".*"),
+      'title' => File.basename(@audio_url, '.*'),
       'assets_path' => "/assets/miniaudio-#{Jekyll::Miniaudio::VERSION}",
       'id' => Random.rand.to_s
-      )
+    )
   end
+end
+
+Jekyll::Hooks.register :site, :after_init do |site|
+  destination_dir = site.config['destination']
+  dest_assets_path = File.join destination_dir, 'assets', "miniaudio-#{Jekyll::Miniaudio::VERSION}"
+  src_asset_path = File.join File.expand_path(__dir__), 'miniaudio', 'h5audio'
+  unless Dir.exist?(dest_assets_path)
+    FileUtils.cp_r(src_asset_path, dest_assets_path)
+  end
+
+  # make sure directory _sitesassets/miniaudio-*.*.* will be kept by jekyll
+  keep_files = site.config['keep_files']
+  asset_path = "assets/miniaudio-#{Jekyll::Miniaudio::VERSION}"
+  keep_files.push(asset_path) unless keep_files.include?(asset_path)
 end
 
 Liquid::Template.register_tag('miniaudio', MiniAudio)
